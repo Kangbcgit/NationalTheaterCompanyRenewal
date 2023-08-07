28c2d5518de1ee29d5716dd675c155a3719ac257
Kangbcgit <Kangbcgit@gmail.com>
2023-08-07 17:56:24 +0900

    텍스트 박스만 추가하면 play section finished

--- src/Component/CircleAni/CircleAni.jsx
+++ src/Component/CircleAni/CircleAni.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components'
 import { Wrapper } from './../Common/Common'
+import { throttle } from 'lodash'
 
 const Parent =styled.div`
   position: absolute;
@@ -11,8 +12,6 @@ const Parent =styled.div`
 
   width: 1000px;
   height: 1000px;
-
-  
 `;
 const Circle = styled.div`
   position: absolute;
@@ -57,38 +56,73 @@ const Circle = styled.div`
 `;
 
 export default class CircleAni extends Component {
-  
   constructor(props) {
     super(props);
     this.state = {
-      deg: 0,
-      step: 0,
-      dummy:[1, 2, 3, 4, 5]
+      deg: 0, // 현재 부모 rotate deg
+      step: 0, //중앙으로부터 멀어진 양 max 2
+      dummy:[1, 2, 3, 4, 5], //임시로 map 돌리기 위한~ item 번호들
+      frontView: 3,
+      backView: 3,
+      images: [
+        'images/play/play1.png',
+        'images/play/play2.png',
+        'images/play/play3.png',
+        'images/play/play4.png',
+        'images/play/play5.png',
+      ],
+      isActiveUp: false,
+      isActiveDown: false,
     }
   }
-
-  Effected = (e) => {
+  componentDidMount() {
+    this.throttledEffected = throttle(this.Effected, 1000, {
+      trailing: false,
+    });
+  }
+  Effected = e => {
     const { deg } = this.state;
     const { dataset } = e.target;
     if (dataset.deg > deg) {
       this.setState({deg: deg + Math.abs(dataset.deg - deg), step: this.step + (Math.abs(dataset.deg - deg) / 30)});
+      this.imageUpAnimation(e);
     } else if (dataset.deg < deg) {
       this.setState({deg: deg - Math.abs(dataset.deg - deg), step: this.step - (Math.abs(dataset.deg - deg) / 30)});
+      this.imageDownAnimation(e);
     }
+  }
+  imageUpAnimation = e => {
+    this.setState({backView: +e.target.dataset.order, isActiveUp: !this.state.isActiveUp});
+    setTimeout(() => {
+      this.setState({frontView: +e.target.dataset.order, isActiveUp: !this.state.isActiveUp})
+    }, 1000)
+  }
+  imageDownAnimation = e => {
+    this.setState({backView: +e.target.dataset.order, isActiveDown: !this.state.isActiveDown});
+    setTimeout(() => {
+      this.setState({frontView: +e.target.dataset.order, isActiveDown: !this.state.isActiveDown})
+    }, 1000)
   }
 
   render() {
     return (
       <>
         <Wrapper>
-          <img src="images/play/play3.png" alt="" />
+          <div className="wrapImg">
+            <img src={`images/play/play${this.state.backView}.png`} alt="" />
+          </div>
+          <div className={`wrapImg ${this.state.isActiveUp === true ? 'activeUp' : this.state.isActiveDown === true ? 'activeDown' : ""}`} >
+            <img src={`images/play/play${this.state.frontView}.png`} alt="" />
+          </div>
           <Parent deg={`${this.state.deg}deg`}>
             <Circle>
               {this.state.dummy.map((item, index) => {
                 const itemCalc = item - 3;
                 const degCalc = (itemCalc) * 30;
                 return(
-                  <div data-deg={degCalc} style={{opacity: this.state.deg !== degCalc ? `${1 / ((Math.abs(this.state.deg - degCalc) / 30) + 1)}` : '1', fontSize: this.state.deg !== degCalc ? '1.5rem' : '1.7rem'}} onClick={this.Effected}>메뉴 {item}번 입니다.</div>//
+                  <div data-deg={degCalc} data-order={item} style={{opacity: this.state.deg !== degCalc ? `${1 / ((Math.abs(this.state.deg - degCalc) / 30) + 1)}` : '1', fontSize: this.state.deg !== degCalc ? '1.5rem' : '1.7rem'}} onClick={e => {
+                    this.throttledEffected(e);
+                  }}>메뉴 {item}번 입니다.</div>//
                 );
               })}
             </Circle>
--- src/Component/Common/Common.jsx
+++ src/Component/Common/Common.jsx
@@ -7,15 +7,45 @@ export const Wrapper = styled.div`
   height: 100vh;
 
   overflow: hidden;
-  &>img {
+  &>.wrapImg {
     position: absolute;
-    z-index: -1;
-
     width: 100%;
     height: 100%;
-    object-fit: cover;
-    
-    filter: brightness(.6);
+
+    overflow: hidden;
+    &.activeUp {
+      animation: clipUp 1s forwards;
+      @keyframes clipUp {
+        0% {
+          clip-path: polygon(-100% 0, 100% 0, 100% 200%, 0 100%);
+        }
+        100% {
+          clip-path: polygon(100% 0, 100% 0, 100% 0, 100% 0);
+        }
+      }
+    }
+    &.activeDown {
+      animation: clipDown 1s forwards;
+
+      @keyframes clipDown {
+      0% {
+        clip-path: polygon(0 -100%, 100% 0, 200% 100%, 0 100%);
+      }
+      100% {
+        clip-path: polygon(0 100%, 0 100%, 0 100%, 0 100%);
+      }
+    }
+    }
+    &>img {
+      position: absolute;
+      z-index: -1;
+
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+      
+      filter: brightness(.6);
+    }
   }
 `;
 
