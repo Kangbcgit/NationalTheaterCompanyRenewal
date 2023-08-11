4e1d7df4bc1ee3a715206de0898e2b6197c1195b
Isabella-Kim <jeonghyeon00777@gmail.com>
2023-08-11 15:53:53 +0900

    thirdCommit

--- src/Component/SectionCatchPhrase/SectionCatchPhrase.jsx
+++ src/Component/SectionCatchPhrase/SectionCatchPhrase.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const SectionCatchPhrase = () => {
+  const [position, setPosition] = useState();
+
   return (
     <div className="SectionCatchPhrase">
       <header>
