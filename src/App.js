a7266e00b96fe0681075cf0332aded9654ee0038
Woon <85141928+DdaaaaaatG@users.noreply.github.com>
2023-08-14 17:30:10 +0900

    연구중

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import { GlobalStyle } from './Component/Common/Common';
-import SectionAudition from './Component/SectionAudition/SectionAudition';
-import SectionPlay from './Component/SectionPlay/SectionPlay';
+import Logo from './animation/Logo';
+import { gsap } from "gsap";
+import { useEffect, useRef, useState,useLayoutEffect } from "react";
+import styled from "styled-components";
+
+
+// const SvgLog =styled(Logo)``;
+
+// const Wrapper = styled.div`
+//   width: 100%;
+//   height:100vh;
+//   display: flex; 
+//   justify-content: center;
+//   align-items: center;
+//   &>svg{
+//     width:50%; height:50%;
+//   }
+// `;
 
 function App() {
   return (
-    <>
-      <GlobalStyle/>
-      <SectionAudition />
-    </>
+      <Logo/>
   );
 }
 
