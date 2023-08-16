044845a81c2e3ef5901e822bbd2504b3aaedbe74
Woon <85141928+DdaaaaaatG@users.noreply.github.com>
2023-08-16 17:45:49 +0900

    카드 섹션 제작중

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 import Logo from './animation/Logo';
 import Main from './animation/Main';
 import Create from './animation/Create';
+/* 수정해야할거
 
+  1. 로딩 하는 도중에 마우스 스크롤 안돼도록 막아두기
+  2. 새로고침하면 맨위 섹션으로 돌아가서 로딩되도록 
+
+*/
 function App() {
   const [logoVisible, setLogoVisible] = useState(true);
 
--- src/animation/Create.js
+++ src/animation/Create.js
@@ -1,8 +1,16 @@
-import React from 'react'
+/* eslint-disable */
 import { gsap} from "gsap";
-import { useEffect} from "react";
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import React, { useEffect} from "react";
 import styled from "styled-components";
-import { ReactComponent as Hamburger } from '../create/01poster_작가.svg'
+/* 
+1.크리에이트 섹션 도달하면 고정되고
+2.고정된 상태에서 스크롤 하면
+3.포스터가 한장씩 날라감
+4.단 날라가는 효과는 스크롤 내리면서 같이 날라가는게 아니라, 스크롤하고 한장 바로 날라가게
+
+add class를 사용 해야 할지도 모름..
+*/
 const CreateWrap = styled.div`
   width: 100%;
   height: 100vh;
@@ -11,10 +19,27 @@ const CreateWrap = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: gray;
+
+`;
+const CardUl = styled.ul`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &>li{
+    position:absolute;
+    }
 `;
 function Create() {
   return (
-    <CreateWrap>Create</CreateWrap>
+    <CreateWrap>
+      <CardUl>
+        <li><img src={`${process.env.PUBLIC_URL}/create/poster-작가.png`} /></li>
+        <li><img src={`${process.env.PUBLIC_URL}/create/poster-연출.png`} /></li>
+        <li><img src={`${process.env.PUBLIC_URL}/create/poster-희곡.png`} /></li>
+      </CardUl>
+    </CreateWrap>
   )
 }
 
